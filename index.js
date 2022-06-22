4d7e9369d09b92db8d964dee8dc999691c9071d5
Chance Espinosa <chance.dev093@gmail.com>
2022-06-21 20:51:38 -0700

    Add computerPlay() function

--- index.js
+++ index.js
@@ -21,3 +21,17 @@
 
 // PLAY BEST TO 5
 
+function computerPlay() {
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
+    switch (randomNumber) {
+        case 1:
+            return 'Rock';
+            break;
+        case 2:
+            return 'Paper';
+            break;
+        case 3:
+            return 'Scissors';
+            break;
+    }
+}
