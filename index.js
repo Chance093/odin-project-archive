86db184191baeccde8e9cbb6220211e8c569f206
Chance Espinosa <chance.dev093@gmail.com>
2022-06-21 21:36:16 -0700

    Add computerPlay() function

--- index.js
+++ index.js
@@ -25,13 +25,46 @@ function computerPlay() {
     const randomNumber = Math.floor(Math.random() * 3) + 1;
     switch (randomNumber) {
         case 1:
-            return 'Rock';
+            return 'rock';
             break;
         case 2:
-            return 'Paper';
+            return 'paper';
             break;
         case 3:
-            return 'Scissors';
+            return 'scissors';
             break;
     }
 }
+
+const playerSelection = prompt('Rock... Paper... Scissors... SHOOT!').toLowerCase();
+const computerSelection = computerPlay();
+
+function playGame(user, computer) {
+    if (user === 'rock') {
+        if (computer === 'scissors') {
+            return 'YOU WIN! ROCK BEATS SCISSORS!';
+        } else if (computer === 'paper') {
+            return 'YOU LOSE! PAPER BEATS ROCK!';
+        } else {
+            return 'YOU TIED!';
+        }
+    } else if (user === 'paper') {
+        if (computer === 'rock') {
+            return 'YOU WIN! PAPER BEATS ROCK!';
+        } else if (computer === 'scissors') {
+            return 'YOU LOSE! SCISSORS BEATS PAPER!';
+        } else {
+            return 'YOU TIED!';
+        }
+    } else if (user === 'scissors') {
+        if (computer === 'paper') {
+            return 'YOU WIN! SCISSORS BEATS PAPER!';
+        } else if (computer === 'rock') {
+            return 'YOU LOSE! ROCK BEATS SCISSORS!';
+        } else {
+            return 'YOU TIED!';
+        }
+    }
+}
+console.log(playGame(playerSelection, computerSelection))
+
