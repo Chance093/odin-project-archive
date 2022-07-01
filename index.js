23352dd45958ac463eed1c363c2c9382aac5bff7
Chance Espinosa <chance.dev093@gmail.com>
2022-06-30 22:44:34 -0700

    Add hover event listener that paints

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const container = document.querySelector('.container');
 
-for (let i = 1; i <= 16; i++) {
-    const rowDiv = document.createElement('div');
-    rowDiv.classList.add('start', 'rowDiv');
-    container.appendChild(rowDiv);
-    for (let j = 1; j <= 16; j++) {
-        const columnDiv = document.createElement('div');
-        columnDiv.classList.add('start', 'columnDiv');
-        rowDiv.appendChild(columnDiv);
+function createCanvas() {
+    for (let i = 1; i <= 16; i++) {
+        const rowDiv = document.createElement('div');
+        rowDiv.classList.add('start', 'rowDiv');
+        container.appendChild(rowDiv);
+        for (let j = 1; j <= 16; j++) {
+            const columnDiv = document.createElement('div');
+            columnDiv.classList.add('start', 'columnDiv');
+            rowDiv.appendChild(columnDiv);
+            columnDiv.addEventListener('mouseover', () => {
+                columnDiv.classList.add('paint');
+            });
+        }
     }
 }
+createCanvas();
+
+
