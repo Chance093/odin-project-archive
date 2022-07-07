411d551436e64e26fbd17f73fcb237cd72737f9f
Chance Espinosa <chance.dev093@gmail.com>
2022-07-06 17:42:19 -0700

    Add clickable button logic

--- index.js
+++ index.js
@@ -18,3 +18,18 @@ function operate(a, b, math) {
     return math(a, b);
 }
 
+const digitBtns = document.querySelectorAll('.digits button');
+const operatorBtns = document.querySelectorAll('.operators button');
+
+
+digitBtns.forEach((button) => {
+    button.addEventListener('click', () => {
+        console.log(button.innerText);
+    })
+})
+
+operatorBtns.forEach((button) => {
+    button.addEventListener('click', () => {
+        console.log(button.innerText);
+    })
+})
