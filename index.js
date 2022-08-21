5c0b0865a875d736c46dafe798d5195f74fb99aa
Chance093 <chance.dev093@gmail.com>
2022-08-21 14:57:33 -0700

    Add GameEnd Module
    Add _player1Wins(), _player2Wins(), and _tieGame() functions and move
    checkWinner() function into GameEnd module

--- index.js
+++ index.js
@@ -9,36 +9,56 @@ const Gameboard = (function () { // Gameboard Module
             let cell = document.querySelector(`.cell${index}`)
             cell.textContent = element;
         })
-        _checkWinner();
+        GameEnd.checkWinner();
     };
 
-    const _checkWinner = () => { // Checks if anyone has won after every move
-        if ((gameboard[0] === 'X' && gameboard[1] === 'X' && gameboard[2] === 'X') ||
-            (gameboard[3] === 'X' && gameboard[4] === 'X' && gameboard[5] === 'X') ||
-            (gameboard[6] === 'X' && gameboard[7] === 'X' && gameboard[8] === 'X') ||
-            (gameboard[0] === 'X' && gameboard[3] === 'X' && gameboard[6] === 'X') ||
-            (gameboard[1] === 'X' && gameboard[4] === 'X' && gameboard[7] === 'X') ||
-            (gameboard[2] === 'X' && gameboard[5] === 'X' && gameboard[8] === 'X') ||
-            (gameboard[0] === 'X' && gameboard[4] === 'X' && gameboard[8] === 'X') ||
-            (gameboard[2] === 'X' && gameboard[4] === 'X' && gameboard[6] === 'X')) {
-            console.log('You Win!')
-        } else if ((gameboard[0] === 'O' && gameboard[1] === 'O' && gameboard[2] === 'O') ||
-            (gameboard[3] === 'O' && gameboard[4] === 'O' && gameboard[5] === 'O') ||
-            (gameboard[6] === 'O' && gameboard[7] === 'O' && gameboard[8] === 'O') ||
-            (gameboard[0] === 'O' && gameboard[3] === 'O' && gameboard[6] === 'O') ||
-            (gameboard[1] === 'O' && gameboard[4] === 'O' && gameboard[7] === 'O') ||
-            (gameboard[2] === 'O' && gameboard[5] === 'O' && gameboard[8] === 'O') ||
-            (gameboard[0] === 'O' && gameboard[4] === 'O' && gameboard[8] === 'O') ||
-            (gameboard[2] === 'O' && gameboard[4] === 'O' && gameboard[6] === 'O')) {
-            console.log('You Lose!')
-        } else if (gameboard[0] && gameboard[1] && gameboard[2] &&
-            gameboard[3] && gameboard[4] && gameboard[5] &&
-            gameboard[6] && gameboard[7] && gameboard[8]) {
-            console.log('You Tied!')
+
+
+    return { gameboard, updateGameboard };
+
+})();
+
+const GameEnd = (function () {
+
+    const _player1Wins = () => {
+        console.log('Player 1 Wins');
+    }
+
+    const _player2Wins = () => {
+        console.log('Player 2 Wins');
+    }
+
+    const _tieGame = () => {
+        console.log('Its a draw!');
+    }
+
+    const checkWinner = () => { // Checks if anyone has won after every move
+        if ((Gameboard.gameboard[0] === 'X' && Gameboard.gameboard[1] === 'X' && Gameboard.gameboard[2] === 'X') ||
+            (Gameboard.gameboard[3] === 'X' && Gameboard.gameboard[4] === 'X' && Gameboard.gameboard[5] === 'X') ||
+            (Gameboard.gameboard[6] === 'X' && Gameboard.gameboard[7] === 'X' && Gameboard.gameboard[8] === 'X') ||
+            (Gameboard.gameboard[0] === 'X' && Gameboard.gameboard[3] === 'X' && Gameboard.gameboard[6] === 'X') ||
+            (Gameboard.gameboard[1] === 'X' && Gameboard.gameboard[4] === 'X' && Gameboard.gameboard[7] === 'X') ||
+            (Gameboard.gameboard[2] === 'X' && Gameboard.gameboard[5] === 'X' && Gameboard.gameboard[8] === 'X') ||
+            (Gameboard.gameboard[0] === 'X' && Gameboard.gameboard[4] === 'X' && Gameboard.gameboard[8] === 'X') ||
+            (Gameboard.gameboard[2] === 'X' && Gameboard.gameboard[4] === 'X' && Gameboard.gameboard[6] === 'X')) {
+            _player1Wins();
+        } else if ((Gameboard.gameboard[0] === 'O' && Gameboard.gameboard[1] === 'O' && Gameboard.gameboard[2] === 'O') ||
+            (Gameboard.gameboard[3] === 'O' && Gameboard.gameboard[4] === 'O' && Gameboard.gameboard[5] === 'O') ||
+            (Gameboard.gameboard[6] === 'O' && Gameboard.gameboard[7] === 'O' && Gameboard.gameboard[8] === 'O') ||
+            (Gameboard.gameboard[0] === 'O' && Gameboard.gameboard[3] === 'O' && Gameboard.gameboard[6] === 'O') ||
+            (Gameboard.gameboard[1] === 'O' && Gameboard.gameboard[4] === 'O' && Gameboard.gameboard[7] === 'O') ||
+            (Gameboard.gameboard[2] === 'O' && Gameboard.gameboard[5] === 'O' && Gameboard.gameboard[8] === 'O') ||
+            (Gameboard.gameboard[0] === 'O' && Gameboard.gameboard[4] === 'O' && Gameboard.gameboard[8] === 'O') ||
+            (Gameboard.gameboard[2] === 'O' && Gameboard.gameboard[4] === 'O' && Gameboard.gameboard[6] === 'O')) {
+            _player2Wins();
+        } else if (Gameboard.gameboard[0] && Gameboard.gameboard[1] && Gameboard.gameboard[2] &&
+            Gameboard.gameboard[3] && Gameboard.gameboard[4] && Gameboard.gameboard[5] &&
+            Gameboard.gameboard[6] && Gameboard.gameboard[7] && Gameboard.gameboard[8]) {
+            _tieGame();
         }
     }
 
-    return { gameboard, updateGameboard };
+    return { checkWinner };
 
 })();
 
