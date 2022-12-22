0ce98b708d7f76b4556896fa42c5a84e7e2ee262
Chance093 <chance.dev093@gmail.com>
2022-12-21 18:01:41 -0800

    Add hit() function to Ship factory

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ function Ship(c1, c2) {
     const shipCoords = calcShipCoords();
     let lives = 0;
     let isSunk = false;
+    
     function calcShipCoords() {
         // Take c1 and c2 and find points between them
         // return an array with each point inside of array
@@ -14,8 +15,8 @@ function Ship(c1, c2) {
     }
 
     function hit() {
-        // lives--
-        // checkIfSunk()
+        lives--;
+        checkIfSunk();
     }
 
     return {}
